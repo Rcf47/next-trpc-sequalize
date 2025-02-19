2b43273df971b4772658d45fe445e14ed9f905d9
Vadim Cherepanov <xenos26@gmail.com>
2025-02-19 17:32:28 +0700

    feat(zod): add helpers function for zod schemas

---
+++ src/zod/helpers.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const enumKeys = <T extends Record<string, string>>(e: T) => {
+  return Object.keys(e) as [keyof T, ...Array<keyof T>];
+};
+
+export function zOptional<T extends z.ZodTypeAny>(schema: T) {
+  return z
+    .union([schema, z.literal("")])
+    .transform((value) => (value === "" ? undefined : value))
+    .optional();
+}
+
+export function zOptionalObject<T extends z.ZodRawShape>(schema: T) {
+  return z.object(schema).partial();
+}
