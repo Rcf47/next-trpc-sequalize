1d9ad54ad69432c287ce8ed9302b2fe8d53be293
Vadim Cherepanov <xenos26@gmail.com>
2025-02-21 21:37:15 +0700

    feat(db): add migrate for data base

---
+++ src/db/migrate.ts
@@ -0,0 +1,44 @@
+import { Author, Post } from "@src/models/";
+import sequelize from "./sequelize";
+
+import type { T_PostCreate } from "@src/zod/post";
+import type { T_AuthorCreate } from "@src/zod/author";
+
+(async () => {
+  await sequelize.drop({
+    cascade: true,
+  });
+
+  await Author.sync({ force: true, alter: true });
+  await Post.sync({ force: true, alter: true });
+
+  const authors: T_AuthorCreate[] = [
+    {
+      name: "Jay",
+      email: "jay@example.com",
+      bio: "I am a software engineer.",
+      website: "https://blog.designly.biz",
+    },
+  ];
+
+  await Author.bulkCreate(authors);
+
+  const author = await Author.findOne({
+    where: {
+      email: "jay@example.com",
+    },
+  });
+  if (!author) throw new Error("Author not found.");
+
+  const posts: T_PostCreate[] = [
+    {
+      title: "Hello, World!",
+      content: "This is my first post.",
+      authorId: author.id,
+    },
+  ];
+
+  await Post.bulkCreate(posts);
+
+  process.exit(0);
+})();
